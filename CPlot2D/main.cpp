c68ebcd2ba205da8fff9900603547d24ab0a31f9
amzs <attila@amzsaki.com>
2014-03-26 13:58:07 -0400

    Initial Commit

---
+++ CPlot2D/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPlot2D
+//
+//  Created by AMZs on 2014-03-26.
+//  Copyright (c) 2014 AMZs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
